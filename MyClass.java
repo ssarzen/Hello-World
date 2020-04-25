d4f91fedb3002ee633ae8cafa87a2c145862d09f
Sam Sarzentich <ssarzen@gmail.com>
2020-04-25 13:59:44 +1000

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MyClass 
+{
+  public static void main(String[] args) 
+  {
+    //System.out.println("Hello World");
+	Scanner MyObj = new Scanner(System.in);
+	System.out.println("enter name");
+	
+	String userName = MyObj.nextLine();
+	System.out.println("username is " + userName);
+	
+	
+	
+	
+	
+  }
+}
